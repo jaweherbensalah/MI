43cd550e414e62ecd432b5d9705542fc2c503109
Jaweher Ben Salah <jaweherbensalah@users.noreply.github.com>
2021-03-11 01:32:10 +0100

    first commit

---
+++ PIDEV-MI/src/main/java/tn/mi/spring/PidevMiApplication.java
@@ -0,0 +1,13 @@
+package tn.mi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidevMiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidevMiApplication.class, args);
+	}
+
+}
