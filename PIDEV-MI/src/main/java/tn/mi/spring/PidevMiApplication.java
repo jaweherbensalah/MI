99d1ee030557ad481af8a556c804b9e111af1b9f
Jaweher Ben Salah <jaweherbensalah@users.noreply.github.com>
2021-05-07 09:10:24 +0100

    .

--- PIDEV-MI/src/main/java/tn/mi/spring/PidevMiApplication.java
+++ PIDEV-MI/src/main/java/tn/mi/spring/PidevMiApplication.java
@@ -2,12 +2,40 @@ package tn.mi.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.EnumSet;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
 public class PidevMiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PidevMiApplication.class, args);
 	}
+	@Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        return new ServletRegistrationBean(servlet, "*.jsf");
+    }
+    @Bean
+    public FilterRegistrationBean rewriteFilter() {
+        FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+        rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+                DispatcherType.ASYNC, DispatcherType.ERROR));
+        rwFilter.addUrlPatterns("/*");
+        return rwFilter;
+    }
 
 }
--- PIDEV-MI/src/main/java/tn/mi/spring/entities/User.java
+++ PIDEV-MI/src/main/java/tn/mi/spring/entities/User.java
@@ -1,92 +1,174 @@
+
 package tn.mi.spring.entities;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @Entity
-public class User implements UserDetails {
 
+public class User implements UserDetails {
+	private static final long serialVersionUID =1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-    @Enumerated(EnumType.STRING)
-    private UserRole appUserRole;
-    private Boolean locked = false;
-    private Boolean enabled = false;
+	private Long id;
+	@NotNull(message = "Name cannot be null")
+	private String firstName;
+	@NotNull(message = "Last name cannot be null")
+	private String lastName;
+	@NotNull(message = "City cannot be null")
+	private String city;
+	@NotNull(message = "Nationality cannot be null")
+	private String nationality;
 
+	@NotNull(message = "Email cannot be null")
+	@Email(message = "Email should be valid")
+	private String email;
+	@NotNull(message = "Password cannot be null")
+	@Size(min = 6, max = 200, message 
+	= "Password must be between 6 and 200 characters")
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	private Boolean locked = false;
+	private Boolean enabled = false;
+	
+	private String resetPasswordToken;
+	private int random;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="user")
+	private Set<Fiche> fiche;
+	public Set<Fiche> getFiche() {
+		return fiche;
+	}
+	public void setFiche(Set<Fiche> fiche) {
+		this.fiche = fiche;
+	}
+//*********************ctor pour l'email et tout ça ...
+	//****************************************************
     public User(String firstName,
-                   String lastName,
+                   String lastName,String city,String nationality,
                    String email,
                    String password,
-                   UserRole appUserRole) {
+                   Role role) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.city = city;
+        this.nationality = nationality;
         this.email = email;
         this.password = password;
-        this.appUserRole = appUserRole;
+        this.role = role;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority =
-                new SimpleGrantedAuthority(appUserRole.name());
-        return Collections.singletonList(authority);
-    }
+	public String getCity() {
+		return city;
+	}
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
+	public String getNationality() {
+		return nationality;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public int getRandom() {
+		return random;
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+	public void setRandom(int random) {
+		this.random = random;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return !locked;
-    }
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+	public String getResetPasswordToken() {
+		return resetPasswordToken;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
+	public void setResetPasswordToken(String resetPasswordToken) {
+		this.resetPasswordToken = resetPasswordToken;
+	}
+
+	//****************************************************
+
+
+	@Override
+	@JsonIgnore
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		SimpleGrantedAuthority authority =
+				new SimpleGrantedAuthority(role.name());
+		return Collections.singletonList(authority);
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !locked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 
 	public Long getId() {
 		return id;
@@ -104,13 +186,7 @@ public class User implements UserDetails {
 		this.email = email;
 	}
 
-	public UserRole getAppUserRole() {
-		return appUserRole;
-	}
 
-	public void setAppUserRole(UserRole appUserRole) {
-		this.appUserRole = appUserRole;
-	}
 
 	public Boolean getLocked() {
 		return locked;
@@ -140,11 +216,19 @@ public class User implements UserDetails {
 		this.password = password;
 	}
 
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((appUserRole == null) ? 0 : appUserRole.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
 		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((enabled == null) ? 0 : enabled.hashCode());
 		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
@@ -164,7 +248,7 @@ public class User implements UserDetails {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		if (appUserRole != other.appUserRole)
+		if (role != other.role)
 			return false;
 		if (email == null) {
 			if (other.email != null)
@@ -204,31 +288,72 @@ public class User implements UserDetails {
 		return true;
 	}
 
-	public User(String firstName, String lastName, String email, String password, UserRole appUserRole,
-			Boolean locked, Boolean enabled) {
+	public User(String firstName, String city,String nationality,String lastName, String email, String password, Role role,
+			Boolean locked, Boolean enabled,String resetPasswordToken) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.city = city;
+		this.nationality = nationality;
 		this.email = email;
 		this.password = password;
-		this.appUserRole = appUserRole;
+		this.role = role;
 		this.locked = locked;
 		this.enabled = enabled;
+		this.resetPasswordToken=resetPasswordToken;
 	}
 
-	public User(Long id, String firstName, String lastName, String email, String password, UserRole appUserRole,
-			Boolean locked, Boolean enabled) {
+	public User(Long id, String firstName, String lastName,String city,String nationality, String email, String password, Role role,
+			Boolean locked, Boolean enabled,String resetPasswordToken) {
 		super();
-		this.id = id;
+		this.id = id; 
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.city = city;
+		this.nationality = nationality;
 		this.email = email;
 		this.password = password;
-		this.appUserRole = appUserRole;
+		this.role = role;
 		this.locked = locked;
 		this.enabled = enabled;
+		this.resetPasswordToken=resetPasswordToken;
 	}
 	public User(){}
- 
+
+	public User(Long id,String firstName, String lastName, String city, String nationality, String email, String password,
+			Role role, Boolean enabled) {
+		
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.nationality = nationality;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		//this.locked = locked;
+		this.enabled = enabled;
+		//this.resetPasswordToken=resetPasswordToken;
+		
+	}
+
+	public User(String firstName, String lastName, String city, String nationality, String email, String password,
+			Role role, Boolean enabled) {
+	
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.city = city;
+		this.nationality = nationality;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		//this.locked = locked;
+		this.enabled = enabled;
+		//this.resetPasswordToken=resetPasswordToken;
+		
+	}
+
+
+
 
 }
